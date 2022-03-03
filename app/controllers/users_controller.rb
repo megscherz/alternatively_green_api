f131bef45586edd0466f0ea15c3911831af76cc9
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-03-03 09:10:44 -0600

    adds part of user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      user_name: params[:user_name],
+      email: params[:email],
+      password_digest: params[:password_digest],
+      image_url: params[:image_url],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
