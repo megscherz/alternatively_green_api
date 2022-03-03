2213a80c7889b1787923743c60454073ad960bc7
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-03-03 08:16:44 -0600

    adds routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  has_many: reviews
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
+
+  has_many reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/first_name'
-  get 'users/last_name'
-  get 'users/user_name'
-  get 'users/email'
-  get 'users/password_digest'
-  get 'users/image_url'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/users" => "users#index"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
 end
